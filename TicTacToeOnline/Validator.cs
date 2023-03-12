c30131ca28a23ce139d7a44f0fbeee5045fa23aa
Ryszard Szewczyk <ryszard.szewczyk@enigma.com.pl>
2023-03-12 17:30:35 +0100

    Added validator

---
+++ TicTacToeOnline/Validator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToeOnline
+{
+    public class Validator
+    {
+        private static List<char> illegalCharacters = new List<char>
+        {
+            '\n',
+            '\r',
+            '-',
+            ' '
+        };
+
+        private static Dictionary<Guid, String> illegalWords = new Dictionary<Guid, String>()
+        {
+            { Guid.NewGuid(), "arse" },
+            { Guid.NewGuid(), "arsehead" },
+            { Guid.NewGuid(), "arsehole" },
+            { Guid.NewGuid(), "ass" },
+            { Guid.NewGuid(), "asshole" },
+            { Guid.NewGuid(), "bastard" },
+
+        };
+
+        private Validator() { }
+
+        public static void ValidateNickName(string nickName)
+        {
+            if (String.IsNullOrEmpty(nickName) || ContainsInvalidCharacters(nickName) 
+                    || ContainsInvalidCharacters(nickName) || )
+            {
+                throw new ValidatorException();
+            }
+        }
+
+        private static bool ContainsInvalidCharacters(string nickName)
+        {
+            return nickName.Any(c => illegalCharacters.Contains(c));
+        }
+
+        /// <summary>
+        /// Deconstruct around what?
+        /// Every possible character out of ASCII or normal english alphabet?
+        /// </summary>
+        /// <param name="nickName"></param>
+        private static void DeconstructString(ref String nickName)
+        {
+
+        }
+    }
+}
---
+++ TicTacToeOnline/ValidatorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TicTacToeOnline
+{
+    public class ValidatorException : Exception
+    {
+        public ValidatorException()
+            : this("Invalid nickname")
+        {
+            
+        }
+
+        public ValidatorException(String message)
+            : base(message)
+        {
+
+        }
+    }
+}
